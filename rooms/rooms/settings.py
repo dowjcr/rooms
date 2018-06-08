24d6052df6aab31b3cb220b08dd95c4d6d2409a1
cjoc <cameronoconnor@outlook.com>
2018-06-08 23:42:31 +0100

    Created models to represent room data.

--- rooms/rooms/settings.py
+++ rooms/rooms/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'roomsurvey.apps.RoomsurveyConfig'
 ]
 
 MIDDLEWARE = [
--- rooms/roomsurvey/models.py
+++ rooms/roomsurvey/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
-# Create your models here.
+"""
+MODELS
+Defines database models to be used in this simple survey
+application, to get room metadata.
+"""
+
+## Model to represent a staircase, currently only with a name attribute.
+
+class Staircase(models.Model):
+    staircase_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+## Model to represent a room, with various attributes. Note that
+## size is stored as an Integer, to be divided by 100.
+
+class Room(models.Model):
+    room_id = models.CharField(max_length=10, primary_key=True)
+    name = models.CharField(max_length=30)
+    is_ensuite = models.BooleanField()
+    is_double_bed = models.BooleanField()
+    size = models.IntegerField()
+    staircase = models.ForeignKey(Staircase, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+## Model to represent a room review, written by a student.
+
+class Review(models.Model):
+    review_id = models.AutoField(primary_key=True)
+    author = models.CharField(max_length=30)
+    room_id = models.ForeignKey(Room, on_delete=models.CASCADE)
+    text = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.review_id
