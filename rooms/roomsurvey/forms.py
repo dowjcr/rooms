3b2255371a3bae34e694cf7c651d7facc71a5a4f
cjoc <cameronoconnor@outlook.com>
2018-06-10 16:02:51 +0100

    Room dropdown now reactive to staircase selected.

--- rooms/roomsurvey/forms.py
+++ rooms/roomsurvey/forms.py
@@ -2,8 +2,20 @@ from django import forms
 
 from .models import Staircase, Room
 
+"""
+FORMS
+Defines forms, for example to input room/staircase,
+respond to survey, et cetera.
+"""
+
+# Form to select staircase.
 class StaircaseSelector(forms.Form):
     staircase = forms.ModelChoiceField(queryset=Staircase.objects.all())
 
+
+# Form to select room, having selected staircase.
 class RoomSelector(forms.Form):
-    room = forms.ModelChoiceField(queryset=Room.objects.all())
+    def __init__(self, *args, **kwargs):
+       sc = kwargs.pop('staircase')
+       super(RoomSelector, self).__init__(*args, **kwargs)
+       self.fields["room"] = forms.ModelChoiceField(queryset=Room.objects.all().filter(staircase=sc))
--- rooms/roomsurvey/views.py
+++ rooms/roomsurvey/views.py
@@ -13,41 +13,33 @@ application, to display various metadata.
 """
 
 ## Generic view to show room details.
-
 class RoomDetailView(generic.DetailView):
     model = Room
     template_name = 'roomsurvey/room.html'
 
 
 ## Selector for staircase.
-
 def get_staircase(request):
     if request.method == 'POST':
         form = StaircaseSelector(request.POST)
         if form.is_valid():
             id = request.POST.get('staircase')
             return HttpResponseRedirect('/roomsurvey/staircase/' + str(id) + '/selectroom')
-
     else:
         form = StaircaseSelector()
-
     return render(request, 'roomsurvey/get-staircase.html', {'form': form})
 
 
 ## Selector for room, reactive to previous staircase selection.
-## TODO - implement reaction.
-
 def get_room(request, staircase_id):
     st = get_object_or_404(Staircase, pk=staircase_id)
     if request.method == 'POST':
-        form = RoomSelector(request.POST)
+        form = RoomSelector(request.POST, staircase=st)
         if form.is_valid():
             id = request.POST.get('room')
             return HttpResponseRedirect('../../../roomsurvey/room_details/' + str(id))
-    
     else:
-        form = RoomSelector()
-    
+        form = RoomSelector(staircase=st)
     return render(request, 'roomsurvey/get-room.html', {
         'form' : form,
         'staircase' : st
