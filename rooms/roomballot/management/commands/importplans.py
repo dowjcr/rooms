f55efc66bdc941e8aa6c154f2f31d8e3d2df078a
cjoc <cameronoconnor@outlook.com>
2020-02-09 19:04:49 +0000

    Added command to import plans

--- rooms/roomballot/management/commands/importplans.py
+++ rooms/roomballot/management/commands/importplans.py
@@ -1,5 +1,7 @@
 from roomballot.models import RoomPlan, Room
 from django.core.management.base import BaseCommand, CommandError
+from django.core.files import File
+import os
 
 class Command(BaseCommand):
     help = "Imports per-room floorplans in a directory given as argument."
@@ -8,5 +10,13 @@ class Command(BaseCommand):
         parser.add_argument('path', nargs='+', type=str)
 
     def handle(self, *args, **options):
-        
-        pass
+        with os.scandir('/var/www/ballot.downingjcr.co.uk/static/media/room_plans') as entries:
+            for entry in entries:
+                file = File(open('/var/www/ballot.downingjcr.co.uk/static/media/room_plans/' + entry.name))
+                rooms = Room.objects.filter(identifier=entry.split(".")[0])
+                for room in rooms:
+                    plan = RoomPlan()
+                    plan.room = room
+                    plan.file = file
+                    plan.save()
+                
