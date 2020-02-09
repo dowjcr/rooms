bfba19fe0876b57d11f2498eb4788641e1048f84
cjoc <cameronoconnor@outlook.com>
2020-02-09 19:12:03 +0000

    Fixed bug in room plan import

--- rooms/roomballot/management/commands/importplans.py
+++ rooms/roomballot/management/commands/importplans.py
@@ -3,6 +3,7 @@ from django.core.management.base import BaseCommand, CommandError
 from django.core.files import File
 import os
 
+
 class Command(BaseCommand):
     help = "Imports per-room floorplans in a directory given as argument."
 
@@ -10,13 +11,12 @@ class Command(BaseCommand):
         parser.add_argument('path', nargs='+', type=str)
 
     def handle(self, *args, **options):
-        with os.scandir('/var/www/ballot.downingjcr.co.uk/static/media/room_plans') as entries:
-            for entry in entries:
-                file = File(open('/var/www/ballot.downingjcr.co.uk/static/media/room_plans/' + entry.name))
-                rooms = Room.objects.filter(identifier=entry.split(".")[0])
-                for room in rooms:
-                    plan = RoomPlan()
-                    plan.room = room
-                    plan.file = file
-                    plan.save()
-                
+        entries = os.scandir(options['path'])
+        for entry in entries:
+            file = File(open(options['path'] + '/' + entry.name))
+            rooms = Room.objects.filter(identifier=entry.split(".")[0])
+            for room in rooms:
+                plan = RoomPlan()
+                plan.room = room
+                plan.file = file
+                plan.save()
